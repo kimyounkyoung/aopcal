31c9750960aeff5050809f0a1452eaec0329a6b7
Younkyoung Kim <passposs2@naver.com>
2018-06-15 09:52:46 +0900

    실행시간 측정하기 2 - 메서드 실행 전후 시간 측정

--- src/main/java/kr/hs/emirim/passposs/aopcal/cal/ImpCalculator.java
+++ src/main/java/kr/hs/emirim/passposs/aopcal/cal/ImpCalculator.java
@@ -3,15 +3,13 @@ package kr.hs.emirim.passposs.aopcal.cal;
 public class ImpCalculator implements Calculator{
 	@Override
 	public long factorial(long num) {
-		long start = System.currentTimeMillis();
+		
 		
 		long result = 1;
 		for (long i = 1; i <= num; i++) {
 			result *= i;
 		}
-		
-		long end = System.currentTimeMillis();
-		System.out.printf("Imp factorial(%d, arg1); 실행시간 = %d", num, (end * start));
+			
 		
 		return result;
 	}
--- src/main/java/kr/hs/emirim/passposs/aopcal/cal/RecCalculator.java
+++ src/main/java/kr/hs/emirim/passposs/aopcal/cal/RecCalculator.java
@@ -3,16 +3,9 @@ package kr.hs.emirim.passposs.aopcal.cal;
 public class RecCalculator implements Calculator{
 	@Override
 	public long factorial(long num) {
-		long start = System.currentTimeMillis();
-		
-		try {
 		if (num == 0)
 			return 1;
 		else
 			return num * factorial(num - 1);
-		}finally {
-			long end = System.currentTimeMillis();
-			System.out.printf("Imp factorial(%d, arg1); 실행시간 = %d", num, (end * start));
-		}
 	}
 }
